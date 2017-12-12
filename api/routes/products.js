1e0fbf8494f9a6b8d4116ef7660fdcb7c61268c4
Andrii Savchuk <andriksr@gmail.com>
2017-12-12 23:18:53 +0200

    refactor patch and delete methods

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,24 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'GET requests to /products'
-  })
+  Product.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      // if (docs.length >= 0) {
+        res.status(200).json(docs);
+      // } else {
+      //   res.status(404).json({
+      //     message: 'No enries found'
+      //   });
+      // }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+    });
+  });
 });
 
 router.post('/', (req, res, next) => {
@@ -30,7 +45,7 @@ router.post('/', (req, res, next) => {
     .catch(err => {
       console.log(err);
       res.status(500).json({error: err});
-    });
+  });
 });
 
 router.get('/:productId', (req, res, next) => {
@@ -51,14 +66,36 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated product'
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({_id : id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+    });
   });
 });
 
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Deleted product'
+  const id = req.params.productId;
+  Product.remove({_id : id })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+    });
   });
 });
 
