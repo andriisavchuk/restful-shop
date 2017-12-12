fa38b36fabe63ca5f27d36a9f663e344e3c8ecff
Andrii Savchuk <andriksr@gmail.com>
2017-12-12 22:15:36 +0200

    added Product schema

---
+++ api/models/product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: String,
+  price: Number,
+  currency: String,
+  description: String
+});
+
+module.exports = mongoose.model('Product', productSchema);
