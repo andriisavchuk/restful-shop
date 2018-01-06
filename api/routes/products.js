050c58b4c1bd88c4cd14a92cf3ec7e5b4f3e95df
Andrii Savchuk <andriksr@gmail.com>
2018-01-06 12:52:16 +0200

    added file uploading functionality

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,7 +2,33 @@ const express   = require('express');
 const router    = express.Router();
 const mongoose  = require('mongoose');
 const multer    = require('multer');
-const upload    = multer({dest: 'uploads/'});
+
+// File storage logic
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload    = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+    fileFilter: fileFilter
+  }
+});
 
 const Product   = require('../models/product');
 
@@ -11,19 +37,19 @@ router.get('/', (req, res, next) => {
   Product.find()
     .select('_id name price currency description')
     .exec()
-    .then(products => {
+    .then(docs => {
       const response = {
-        count: products.length,
-        products: products.map(doc => {
+        count: docs.length,
+        products: docs.map(doc => {
           return {
-            _id: product._id,
-            name: product.name,
-            price: product.price,
-            currency: product.currency,
-            description: product.description,
+            _id: doc._id,
+            name: doc.name,
+            price: doc.price,
+            currency: doc.currency,
+            description: doc.description,
             request: {
               type: 'GET',
-              url: 'http://localhost:3000/products/' + product._id
+              url: 'http://localhost:3000/products/' + doc._id
             }
           }
         })
@@ -45,7 +71,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', upload.single('productImage'), (req, res, next) => {
-  console.log(req.file);
+  // console.log(req.file);
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
