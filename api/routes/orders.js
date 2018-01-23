7c9b01ac4d12c37bfa14a6701c0a9ae8edc09590
Andrii Savchuk <andriksr@gmail.com>
2018-01-23 21:42:50 +0200

    added authentication middleware

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,12 +1,13 @@
 const express   = require('express');
 const router    = express.Router();
 const mongoose  = require('mongoose');
+const checkAuth = require('../middleware/auth');
 
 const Order     = require('../models/order');
 const Product   = require('../models/product');
 
 // Orders CRUD Functionality
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
   Order.find()
     .select('product quantity _id')
     .populate('product', 'name price currency description')
@@ -34,7 +35,7 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
   Product.findById(req.body.productId)
     .then(product => {
 
@@ -75,7 +76,7 @@ router.post('/', (req, res, next) => {
   });
 });
 
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId', checkAuth, (req, res, next) => {
   Order.findById(req.params.orderId)
     .select('product quantity _id')
     .populate('product', 'name price currency description')
@@ -101,7 +102,7 @@ router.get('/:orderId', (req, res, next) => {
   });
 });
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
   Order.remove({_id: req.params.orderId})
   .exec()
   .then(result => {
